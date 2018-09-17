1e59dff4ab0fd0bef5e5c558ca209d32ab6e23af
Unknown <maxanto@gmail.com>
2018-09-17 18:09:24 -0300

    Agregado delay
    Agregué delay en PDF_BP.m
    Falta pdobar que funcione.

--- PDF_BP.m
+++ PDF_BP.m
@@ -1,14 +1,15 @@
-function PDF = PDF_BP(x, D)
+function PDF = PDF_BP(x, D, tau)
 
 %x es el vector de entrada
 %D es la dimensión de embedding
+%tau es el retardo
 
 n = length(x);
 FactorialBasis = fliplr(factorial(0:D-1));
 PDF = zeros(1,factorial(D));
 
-for i_vec = 1:n-D+1
-    x_emb = x(i_vec:i_vec + D - 1);
+for i_vec = 1:n-D*tau+1
+    x_emb = x(i_vec:tau:i_vec + D*tau - 1);
     x_comp = zeros(1, D);
     for i_D = 1:D-1
         for i_comp = i_D+1:D
